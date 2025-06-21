d648c45b307ba392d49954fef0fcca052691e961
Hiromasa Cross <evor861@gmail.com>
2025-06-21 03:27:49 +0000

    [automatic commit] 2025-06-21 03:27:49 UTC 180.150.112.93

--- part2/app.js
+++ part2/app.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const path = require('path');
-require('dotenv').config();
+const session = require('express-session');
+
 
 const app = express();
 
-// Middleware
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/public')));
+
+// Session setup
+app.use(session({
+  secret: 'supersecretkey', // In production, use process.env.SESSION_SECRET
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: false, // Set true if using HTTPS
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60 // 1 hour
+  }
+}));
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
@@ -15,5 +29,4 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
-// Export the app instead of listening here
 module.exports = app;
