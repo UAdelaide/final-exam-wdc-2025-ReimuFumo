0c5a85dd984190ab5e1d616df1874dd5b8be019a
Hiromasa Cross <evor861@gmail.com>
2025-06-21 02:03:15 +0000

    [automatic commit] 2025-06-21 02:03:15 UTC 180.150.112.93

--- part1/routes/walker.js
+++ part1/routes/walker.js
@@ -26,7 +26,7 @@ router.get('/walkrequest/open', async (req, res) => {
 
 router.get('/walkers/summary', async (req, res) => {
   const [rows] = await db.query(`
-    SELECT m.MessageText, m.SentAt, bi.Title, u.Name AS BuyerName
+    SELECT ws.
     FROM Messages m
     JOIN BookListings bl ON m.BookID = bl.BookID
     JOIN BookInfo bi ON bl.BookInfoID = bi.BookInfoID
