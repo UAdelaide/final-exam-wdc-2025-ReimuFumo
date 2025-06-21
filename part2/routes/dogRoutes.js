de3205bec13f81e5718246daf2f75103ca4d7836
Hiromasa Cross <evor861@gmail.com>
2025-06-21 05:12:54 +0000

    [automatic commit] 2025-06-21 05:12:54 UTC 180.150.112.93

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -18,5 +18,15 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/list', async (req, res) => {
+  try {
+    const [dogs] = await db.query('SELECT dog_id, name, breed, size FROM Dogs');
+    res.json(dogs);
+  } catch (err) {
+    console.error('Failed to get dogs:', err);
+    res.status(500).json({ error: 'Failed to fetch dogs' });
+  }
+});
+
 
 module.exports = router;
