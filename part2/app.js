4e1ec9fb622c589299386472d59522b83fa61d54
Hiromasa Cross <evor861@gmail.com>
2025-06-21 03:42:03 +0000

    [automatic commit] 2025-06-21 03:42:03 UTC 180.150.112.93

--- part2/app.js
+++ part2/app.js
@@ -1,32 +1,24 @@
 const express = require('express');
-const path = require('path');
 const session = require('express-session');
-
+const path = require('path');
+const userRoutes = require('./routes/userRoutes');
+const walkRoutes = require('./routes/walkRoutes');
 
 const app = express();
 
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Session setup
 app.use(session({
   secret: 'supersecretkey',
   resave: false,
   saveUninitialized: false,
-  cookie: {
-    secure: false, // Set true if using HTTPS
-    httpOnly: true,
-    maxAge: 1000 * 60 * 60 // 1 hour
-  }
 }));
 
-// Routes
-const walkRoutes = require('./routes/walkRoutes');
-const userRoutes = require('./routes/userRoutes');
-
-app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/walks', walkRoutes);
 
-module.exports = app;
+// Start server
+app.listen(8081, () => {
+  console.log('Server running at http://localhost:8081');
+});
