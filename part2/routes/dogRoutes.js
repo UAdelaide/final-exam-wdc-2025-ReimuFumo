cadd94a5bb9b03c7ce83ed997e2a03b68a6db6db
Hiromasa Cross <evor861@gmail.com>
2025-06-21 04:51:55 +0000

    [automatic commit] 2025-06-21 04:51:55 UTC 180.150.112.93

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db'); // adjust to your actual DB connection
+const db = require('../models/db'); // adjust to your actual DB connection
 
 router.get('/', async (req, res) => {
   if (!req.session.user || req.session.user.role !== 'owner') {
