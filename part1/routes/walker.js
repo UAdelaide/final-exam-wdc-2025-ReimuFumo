7551f371648b2d7acb0aecb34fd3bd1df3269d63
Hiromasa Cross <evor861@gmail.com>
2025-06-21 02:05:03 +0000

    [automatic commit] 2025-06-21 02:05:03 UTC 180.150.112.93

--- part1/routes/walker.js
+++ part1/routes/walker.js
@@ -26,7 +26,7 @@ router.get('/walkrequest/open', async (req, res) => {
 
 router.get('/walkers/summary', async (req, res) => {
   const [rows] = await db.query(`
-    SELECT u.username AS walker_username, COUNT
+    SELECT u.username AS walker_username, COUNT(r.rating) AS total_ratings, ROUND(AVG(r.rating))
     FROM Messages m
     JOIN BookListings bl ON m.BookID = bl.BookID
     JOIN BookInfo bi ON bl.BookInfoID = bi.BookInfoID
