89338637bb189e04c932fcffe9a17878a0844052
Hiromasa Cross <evor861@gmail.com>
2025-06-21 04:34:32 +0000

    [automatic commit] 2025-06-21 04:34:32 UTC 180.150.112.93

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -62,4 +62,18 @@ router.post('/login', async (req, res) => {
   }
 });
 
+//Logging out
+router.post('/logout', (req, res) => {
+  req.session.destroy(err => {
+    if (err) {
+      console.error('Logout error:', err);
+      return res.status(500).json({ error: 'Could not log out' });
+    }
+
+    res.clearCookie('connect.sid'); // Clear session cookie
+    res.json({ message: 'Logged out successfully' });
+  });
+});
+
+
 module.exports = router;
