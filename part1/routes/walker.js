8cfcecf8dc9371beed07ae4a26d463c1ea53da62
Hiromasa Cross <evor861@gmail.com>
2025-06-21 01:53:56 +0000

    [automatic commit] 2025-06-21 01:53:56 UTC 180.150.112.93

--- part1/routes/walker.js
+++ part1/routes/walker.js
@@ -2,10 +2,9 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db');
 
-var CURRENT_BUYER_ID = 1;
-var CURRENT_SELLER_ID = 2;
 
-router.get('/items', async (req,res) => {
+
+router.get('/dogs', async (req,res) => {
    const [rows] = await db.query(`
   SELECT bl.BookID, bi.Title, u.Name AS SellerName, bl.SellerID
   FROM BookListings bl
